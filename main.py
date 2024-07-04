706a5f57b45ab208c6215a11578172f07183aaf5
khangcao0504 <khangcao2k9@gmail.com>
2024-07-04 15:32:05 +0700

    Check entries for real / imaginary number. Error message for wrong formatting Focus on entry with wrong formatting

--- main.py
+++ main.py
@@ -14,18 +14,34 @@ class Number:
     # sum = num1 + num2
     # result.delete(0, END)
     # result.insert(END, sum)
+def data_type_error():
+    error_label.config(text="Please check number formatting")
 def get_entry():
     for key in entries:
         raw_entry = entries[key].get()
         no_space_entry = "".join(raw_entry.split())
         if no_space_entry.isdigit():
             print(f"{no_space_entry} is real")
-        elif "i" not in no_space_entry:
-            print(no_space_entry)
+            error_label.config(text="")
+        else:
+            match = re.match(r"(?P<real>[0-9]{1,})(?P<imaginary>[+-]{1}[0-9]{1,})[i]$", no_space_entry)
+            if match:
+                entry = Number(real=match.group("real"),imaginary=match.group("imaginary"))
+                print(f"{int(entry.real)}, {int(entry.imaginary)}")
+                error_label.config(text="")
+            else:
+                entries[key].focus()
+                data_type_error()
 
-a = "123-45i"
-print(re.match("[0-9]{1,}[+-]{1}[0-9]{1,}[i]$",a))
-# imaginary regex format: "[0-9]{1,}[+-]{1}[0-9]{1,}[i]$"
+a = "+45"
+print(int(a))
+# match = re.match(r"(?P<real>[0-9]{1,})(?P<imaginary>[+-]{1}[0-9]{1,})[i]$", a)
+# if match:
+#     print(match.group("imaginary"))
+# else:
+#     print("nope")
+# imaginary regex format (simple): "[0-9]{1,}[+-]{1}[0-9]{1,}[i]$"
+# imaginary regex format (better): r"(?P<real>[0-9]{1,}) (?P<imaginary>[+-]{1}[0-9]{1,})[i]$"
 
 
 # ============= GUI ============= #
@@ -65,6 +81,9 @@ result.grid(column=4, row=2)
 
 calculate_button = Button(text="Calculate", width=10, command=get_entry)
 calculate_button.grid(column=2, row=3)
+
+error_label = Label(text="", font=("courier", 14))
+error_label.grid(column=2, row=4)
 
 app.mainloop()
 

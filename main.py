70ab50ddba56bb1c43914f8fa2d4aa7fca150116
khangcao0504 <khangcao2k9@gmail.com>
2024-07-04 16:56:02 +0700

    add, subtract, multiply error when no operator is selected

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Number:
         self.real = real
         self.imaginary = imaginary
 
-# ============= Function ============= #
+# ============= Functions ============= #
 def calculate(num1,num2):
     x = num1
     y = num2
@@ -25,10 +25,10 @@ def calculate(num1,num2):
             answer = Number(real= a-c, imaginary= b-d)
         elif operator == "multiply":
             answer = Number(real= a*c - b*d, imaginary= a*d + b*c)
+        else:
+            pass
     except NameError:
         messagebox.showerror(title="No operator", message="Please select an operator")
-    except UnboundLocalError:
-        messagebox.showerror(title="404 notfound", message="Work in progress")
 
     result.delete(0, END)
     if answer.imaginary == 0:
@@ -37,7 +37,6 @@ def calculate(num1,num2):
         result.insert(END, f"{answer.real}+{answer.imaginary}i")
     else:
         result.insert(END, f"{answer.real}{answer.imaginary}i")
-
 
 def get_entry():
     entry_list = []
@@ -58,18 +57,10 @@ def get_entry():
                                                   "- 'a' where a is a Real number\n"
                                                   "- 'a +- bi' where a and b are Real numbers")
                 break
-    calculate(entry_list[0],entry_list[1])
+    calculate(entry_list[0], entry_list[1])
 
-# a = "+45"
-# print(int(a))
-# match = re.match(r"(?P<real>[0-9]{1,})(?P<imaginary>[+-]{1}[0-9]{1,})[i]$", a)
-# if match:
-#     print(match.group("imaginary"))
-# else:
-#     print("nope")
 # imaginary regex format (simple): "[0-9]{1,}[+-]{1}[0-9]{1,}[i]$"
 # imaginary regex format (better): r"(?P<real>[0-9]{1,}) (?P<imaginary>[+-]{1}[0-9]{1,})[i]$"
-
 
 # ============= GUI ============= #
 app = Tk()
@@ -83,9 +74,6 @@ entry_1_label = Label(text="Num 1:", font=("courier", 14))
 entry_1_label.grid(column=0, row=1)
 
 entry_1 = Entry(width=10)
-# entry_1.grid(column=1, row=1)
-
-operator_list = ['add','subtract','multiply','divide']
 
 # Select operator and save it to operator var
 def update_operator(selection):
@@ -93,6 +81,7 @@ def update_operator(selection):
     operator = selection
     print(operator)
 
+operator_list = ['add','subtract','multiply','divide']
 selection = StringVar()
 operator_button = OptionMenu(app, selection, *operator_list, command=update_operator)
 operator_button.grid(column=2, row=1)
@@ -101,9 +90,10 @@ entry_2_label = Label(text="Num 2:", font=("courier", 14))
 entry_2_label.grid(column=3, row=1)
 
 entry_2 = Entry(width=10)
-# entry_2.grid(column=4, row=1)
-entries = {"1":entry_1,
-           "2":entry_2}
+
+# putting entries in dict and displaying them, this is reserved for when you want to add in more entries
+entries = {"1": entry_1,
+           "2": entry_2}
 entries["1"].grid(column=1, row=1)
 entries["2"].grid(column=3, row=1)
 
